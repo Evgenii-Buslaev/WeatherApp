7ca05d68127daa1466879a7b681c4598fd773338
Evgenii Buslaev <eugenebus144@gmail.com>
2022-07-17 19:05:58 +0500

    Get data for current city via API

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-let city = document.getElementById("city");
+let cityName = document.getElementById("city");
 let locationBtn = document.getElementById("location-btn");
 
-let lat;
-let lng;
 
-locationBtn.addEventListener("click", () => {
+let city = {
+  lat: null,
+  lng: null,
+  city_defined: false,
+}
+
+function getLocation() {
   // getting coords of user
   navigator.geolocation.getCurrentPosition(function (position) {
-    lat = position.coords.latitude;
-    lng = position.coords.longitude;
-    console.log(lat);
-    console.log(lng);
+    city.lat = position.coords.latitude;
+    city.lng = position.coords.longitude;
     // defining city with coords
     let url =
       "https://suggestions.dadata.ru/suggestions/api/4_1/rs/geolocate/address";
     let token = "7b0d07bc549fc06afcf392d74f2aa6beb81a784a";
-    let query = { lat: lat, lon: lng };
+    let query = { lat: city.lat, lon: city.lng };
 
     let options = {
       method: "POST",
@@ -27,12 +29,37 @@ locationBtn.addEventListener("click", () => {
       },
       body: JSON.stringify(query),
     };
-
+    // changing city name to current city (without a prefix)
     fetch(url, options)
       .then((response) => response.json())
-      .then(
-        (result) => (city.innerText = result.suggestions[0].data.city_with_type)
-      )
-      .catch((error) => console.log("error", error));
+      .then((result) => {
+        cityName.innerText = result.suggestions[0].data.city_with_type.match(/(?<=\s)\p{Alpha}+/gu)
+        city.city_defined = true
+      })
+      .catch((error) => {
+        console.log("error", error)
+        city.innerText = 'Москва'
+        city.city_defined = true
+      });
   });
-});
+}
+
+// getting user's location name 
+locationBtn.addEventListener("click", getLocation);
+
+window.addEventListener('load', () => {
+  getLocation();
+  let check = setInterval(() => {
+    if (city.city_defined === true) {
+      
+      let key = '6861653a70004c5f944134019221707'
+      // getting current weather data
+      fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=${cityName.innerText}`)
+        .then((response) => response.json())
+        .then(result => console.log(result))
+        .catch((err) => console.log('error', err))
+      clearInterval(check)
+      }
+    }, 100)
+  })
+
